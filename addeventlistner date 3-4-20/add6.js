b0606e971f19c1bce960a3331b33eed48303aeaf
Sanjay Jain <sanjay21121973jain@gmail.com>
2020-04-03 09:42:53 +0530

    Add files via upload

---
+++ addeventlistner date 3-4-20/add6.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+window.addEventListener("load",gg);
+function gg(){
+var a;
+	var a1,b1,c1;
+	
+a=document.querySelectorAll("input");
+		
+		a[3].addEventListener("click",ss);
+		a[4].addEventListener("click",ss1);
+		a[5].addEventListener("click",ss2);
+		a[6].addEventListener("click",ss3);
+		function dd()
+		{
+			a1=Number(a[0].value);
+			b1=Number(a[1].value);
+		}
+	function ss()
+		{
+			
+			dd();
+		    c1=a1+b1;
+			a[2].value=c1;
+		}
+		function ss1()
+		{
+		
+			dd();
+		    c1=a1-b1;
+			a[2].value=c1;
+		}
+		function ss2()
+		{
+			
+			dd();
+		    c1=a1*b1;
+			a[2].value=c1;
+		}
+		function ss3()
+		{
+			
+			dd();
+		    c1=a1/b1;
+			a[2].value=c1;
+		}
+}
